e85ee213d1e0846001c687e2e78a04b0c18cfe86
demonmaike <dmitry.malyshevv@yandex.ru>
2021-10-21 23:15:23 +0300

    добавлен игнор

--- excel_handler.py
+++ excel_handler.py
@@ -6,7 +6,9 @@ class Excel():
     def __init__(self):
         self.table = 'excel/test_cam.xlsx'
         self.elements = pd.read_excel(self.table)
-        self.list_ip = self.elements['ip'].tolist()
+        self.list_ip = self.elements['ip камер'].tolist()
+        #self.list_type = self.elements['Тип камер'].tolist()
+        self.links_photo = self.elements['Ссылки для снятия скриншотов'].tolist()
         self.false_ping_list = []
 
     def print_table(self):
@@ -15,15 +17,17 @@ class Excel():
 
     def ping_ip(self):
         """ прогоняем через пинг все ip адреса"""
-        for i in self.list_ip:
-            ping = os.system('ping ' + i)
+        for ip in self.list_ip:
+            ping = os.system('ping ' + ip)
             print(ping)
             if ping == 0:
                 pass  # добавим цикл
             # для подстановки ссылок и запись
             # подходящей ссылки, если есть подходящая,
-            # запись типа, если нет - пишем в лог для этого
-            # ip нет подходящей ссылки
+            # запись типа
             else:
-                self.false_ping_list.append(i)
+                self.false_ping_list.append(ip)
     
+ #   def _type_cam(self):
+  #      for link in self.links_photo:
+
